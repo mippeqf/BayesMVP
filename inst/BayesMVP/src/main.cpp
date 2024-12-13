4ef2fbc06f99dec05460058093338e9fc311437f
CerulloE1996 <80172000+CerulloE1996@users.noreply.github.com>
2024-12-13 12:52:55 +0000

    Add files via upload

--- inst/BayesMVP/src/main.cpp
+++ inst/BayesMVP/src/main.cpp
@@ -1451,7 +1451,9 @@ Rcpp::List     Rcpp_wrapper_fn_sample_HMC_multi_iter_single_thread(    const int
   HMC_output_single_chain  HMC_output_single_chain_i(n_iter, n_nuisance_to_track, n_params_main, n_us, N);
 
   // std::mt19937 rng(seed);
-  auto rng = std::unique_ptr<dqrng::random_64bit_generator>(dqrng::generator<dqrng::xoshiro256plusplus>(seed)); 
+  // auto rng = std::unique_ptr<dqrng::random_64bit_generator>(dqrng::generator<dqrng::xoshiro256plusplus>(seed)); 
+  //auto rng = dqrng::generator<pcg64>(seed, 0);
+  pcg64 rng(seed, 0);
   
      fn_sample_HMC_multi_iter_single_thread(    HMC_output_single_chain_i,
                                                 result_input,
@@ -1494,138 +1496,6 @@ Rcpp::List     Rcpp_wrapper_fn_sample_HMC_multi_iter_single_thread(    const int
 
 
 
-
-
-
-
-
-#if HAS_BRIDGESTAN_H
-
-
-struct ParamConstrainWorker : public RcppParallel::Worker {
-
-      //// Inputs
-      const std::vector<Eigen::Matrix<double, -1, -1>> &unc_params_trace_input_main;
-      const std::vector<Eigen::Matrix<double, -1, -1>> &unc_params_trace_input_nuisance;
-      const Eigen::VectorXi &pars_indicies_to_track;
-      const int n_params_full;
-      const int n_nuisance;
-      const int n_params_main;
-      const bool include_nuisance;
-      const std::string &model_so_file;
-      const std::string &json_file_path;
-
-      //// Output uses tbb container
-      tbb::concurrent_vector<RcppParallel::RMatrix<double>> all_param_outs_trace_concurrent; // (n_chains);
-
-  // Constructor
-  ParamConstrainWorker( const std::vector<Eigen::Matrix<double, -1, -1>> &unc_params_trace_input_main,
-                        const std::vector<Eigen::Matrix<double, -1, -1>> &unc_params_trace_input_nuisance,
-                        const Eigen::VectorXi  &pars_indicies_to_track,
-                        const int &n_params_full,
-                        const int &n_nuisance,
-                        const int &n_params_main,
-                        const bool &include_nuisance,
-                        const std::string &model_so_file,
-                        const std::string &json_file_path,
-                        std::vector<Rcpp::NumericMatrix>   &all_param_outs_trace)
-                        :
-                          unc_params_trace_input_main(unc_params_trace_input_main),
-                          unc_params_trace_input_nuisance(unc_params_trace_input_nuisance),
-                          pars_indicies_to_track(pars_indicies_to_track),
-                          n_params_full(n_params_full),
-                          n_nuisance(n_nuisance),
-                          n_params_main(n_params_main),
-                          include_nuisance(include_nuisance),
-                          model_so_file(model_so_file),
-                          json_file_path(json_file_path)
-    {
-
-        // Initialize concurrent vector of RMatrix
-        all_param_outs_trace_concurrent = convert_vec_of_RcppMat_to_concurrent_vector(all_param_outs_trace, all_param_outs_trace_concurrent);
-
-    }
-
-        // Thread-local processing
-        void operator()(std::size_t begin, std::size_t end) {
-
-                  // Process assigned chains
-                    std::size_t kk = begin;
-                    {
-
-                      char* error_msg = nullptr;
-                      const int n_iter = unc_params_trace_input_main[0].cols();
-                      const int n_params = n_nuisance + n_params_main;
-                      const int n_params_to_track = pars_indicies_to_track.size();
-
-                      thread_local stan::math::ChainableStack ad_tape;
-                      thread_local stan::math::nested_rev_autodiff nested;
-
-                      // Initialize matrix for this chain
-                      Eigen::Matrix<double, -1, -1> chain_output(n_params_to_track, n_iter);
-
-                        Stan_model_struct Stan_model_as_cpp_struct;
-                        bs_rng* bs_rng_object = nullptr;
-
-                        // Initialize model and RNG once per thread
-                        if (model_so_file != "none" && Stan_model_as_cpp_struct.bs_model_ptr == nullptr) {
-                          Stan_model_as_cpp_struct = fn_load_Stan_model_and_data(model_so_file, json_file_path, 123);
-                          bs_rng_object = bs_rng_construct(123, &error_msg);
-                        }
-
-                        for (int ii = 0; ii < n_iter; ii++) {
-
-                              Eigen::Matrix<double, -1, 1> theta_unc_full_input(n_params);
-                              Eigen::Matrix<double, -1, 1> theta_constrain_full_output(n_params_full);
-    
-                                  theta_unc_full_input.tail(n_params_main) = unc_params_trace_input_main[kk].col(ii);
-    
-                                  if (include_nuisance) {
-                                    theta_unc_full_input.head(n_nuisance) = unc_params_trace_input_nuisance[kk].col(ii);
-                                  } else {
-                                    theta_unc_full_input.head(n_nuisance).array() = 0.0;
-                                  }
-    
-                                  int result = Stan_model_as_cpp_struct.bs_param_constrain( Stan_model_as_cpp_struct.bs_model_ptr,
-                                                                                            true,
-                                                                                            true,
-                                                                                            theta_unc_full_input.data(),
-                                                                                            theta_constrain_full_output.data(),
-                                                                                            bs_rng_object,
-                                                                                            &error_msg);
-    
-                                  if (result != 0) {
-                                    throw std::runtime_error("computation failed: " +
-                                                             std::string(error_msg ? error_msg : "Unknown error"));
-                                  }
-    
-                                  chain_output.col(ii) = theta_constrain_full_output(pars_indicies_to_track);
-
-                        }
-
-                        // Store completed chain
-                        all_param_outs_trace_concurrent[kk] =  fn_convert_EigenMat_to_RMatrix(chain_output,  all_param_outs_trace_concurrent[kk]);
-
-                        // // Clean up thread-local resources if neede
-                        // if (bs_rng_object != nullptr) {
-                        //   bs_rng_destruct(bs_rng_object);
-                        //  }
-                        
-                       // Clean up thread-local resources 
-                       fn_bs_destroy_Stan_model(Stan_model_as_cpp_struct);
-
-
-                  }
-
-
-
-        }
-
-
-};
-
-
-#endif
 
 
 
