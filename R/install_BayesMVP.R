6ef79334eddc937575d1f09ab8b872083d87522b
CerulloE1996 <80172000+CerulloE1996@users.noreply.github.com>
2025-01-11 01:12:30 +0000

    Add files via upload

--- R/install_BayesMVP.R
+++ R/install_BayesMVP.R
@@ -1,110 +1,14 @@
 
 
 
-#' cmdstanr_path
-#' @export
-cmdstanr_path <- function() {
-
-            # Check if the CMDSTAN environment variable is set
-            try({
-                cmdstan_env <- cmdstanr::cmdstan_path()
-                if (!(cmdstan_env %in% c("", " ", "  "))) {
-                  message("Using CMDSTAN path: ", cmdstan_env)
-                  return(cmdstan_env) # Use the value from the environment variable
-                }
-            }, silent = TRUE)
-
-            # Get the user's home directory
-            home_dir <- Sys.getenv(if (.Platform$OS.type == "windows") "USERPROFILE" else "HOME")
-
-            # Check for .cmdstan directory
-            cmdstan_dirs <- Sys.glob(file.path(home_dir, ".cmdstan", "cmdstan-*"))
-
-            if (length(cmdstan_dirs) > 0) {
-              # Sort directories by version (assumes lexicographical sorting works for version strings)
-              recent_dir <- cmdstan_dirs[order(cmdstan_dirs, decreasing = TRUE)][1]
-              message("Found latest cmdstan in .cmdstan: ", recent_dir)
-              return(recent_dir)
-            }
-
-            # Check for cmdstan directory directly under HOME
-            cmdstan_dir <- file.path(home_dir, "cmdstan")
-            if (dir.exists(cmdstan_dir)) {
-              message("Found cmdstan in home directory: ", cmdstan_dir)
-              return(cmdstan_dir)
-            }
-
-            # If no valid path is found
-            stop("CmdStan directory not found. Please install CmdStan or set the CMDSTAN environment variable.")
-
-}
-
-
-
-
-#' bridgestan_path
-#' @export
-bridgestan_path <- function() {
-
-          # Check if the BRIDGESTAN environment variable is already set
-          if (!(Sys.getenv("BRIDGESTAN") %in% c("", " ", "  "))) {
-            message(paste(cat("Bridgestan path found at:"), Sys.getenv("BRIDGESTAN")))
-            return(Sys.getenv("BRIDGESTAN")) # Use the value from the environment variable
-          }
-
-          # Get the user's home directory
-          home_dir <- Sys.getenv(if (.Platform$OS.type == "windows") "USERPROFILE" else "HOME")
-
-          # Define the default paths for BridgeStan v2.5.0
-          default_path <- file.path(home_dir, ".bridgestan", "bridgestan-2.5.0")
-
-          # Check if the default path exists
-          if (dir.exists(default_path) == TRUE) {
-            Sys.setenv(BRIDGESTAN=default_path)
-            message(paste(cat("Bridgestan path found at:"), default_path))
-            return(default_path)
-          }
-
-
-          # If v2.5.0 not available, then fallback to finding the most recent bridgestan directory
-          search_pattern <- file.path(home_dir, ".bridgestan" , "bridgestan-*")
-          available_dirs <- Sys.glob(search_pattern)
-          
-          # Filter for valid version directories and sort by version
-          if (length(available_dirs) > 0) {
-            recent_dir <- available_dirs[order(available_dirs, decreasing = TRUE)][1]
-            Sys.setenv(BRIDGESTAN=recent_dir)
-            message(paste(cat("Bridgestan path found at:"), recent_dir))
-            return(recent_dir)
-          }
-          
-          ## If nothing, then look for plain "bridgestan" dir (i.e. w/o a ".")
-          search_pattern_wo_dot <- file.path(home_dir, "bridgestan")
-          available_dir_wo_dot <- Sys.glob(search_pattern_wo_dot) ; available_dir_wo_dot
-          return(available_dir_wo_dot)
- 
-
-          # If no valid path is found
-          stop("BridgeStan directory not found.")
-
-}
-
-
- 
-
- 
- 
-
-
-
 #' setup_env_pre_install
 #' @export
 setup_env_pre_install <- function() {
 
 
-        # Set brigestan and cmdstanr environment variables / directories
-        bs_dir <- bridgestan_path()
-        cmdstan_dir <- cmdstanr_path()
+        ## Set brigestan and cmdstanr environment variables / directories
+        #### bs_dir <- BayesMVP:::bridgestan_path()
+        cmdstan_dir <- BayesMVP:::cmdstanr_path()
 
         if (.Platform$OS.type == "windows") {
 
@@ -169,7 +73,7 @@ setup_env_post_install <- function() {
 
 
           # Set brigestan and cmdstanr environment variables / directories
-          bs_dir <- bridgestan_path()
+          #### bs_dir <- bridgestan_path()
           cmdstan_dir <- cmdstanr_path()
 
           ## temp_dir <- Sys.getenv("TEMP")
@@ -323,11 +227,15 @@ install_BayesMVP <- function() {
         
         # Install fresh from temp copy
         options(devtools.install.args = c("--no-test-load", "--no-staged-install"))
-        devtools::install(file.path(temp_dir, "BayesMVP"), 
+        path <- file.path(temp_dir, "BayesMVP")
+        ## Rcpp::compileAttributes(path)  
+        devtools::install(path, 
                           args = c("--no-test-load", "--no-staged-install"),
                           dependencies = FALSE,  # Prevent namespace cycling
                           quiet = FALSE, 
-                          force = TRUE)
+                          force = TRUE
+                         #   quick = TRUE
+                          )
         
         
         try({   setup_env_post_install()  }) # setup env again before loading 
--- R/zzz_outer.R
+++ R/zzz_outer.R
@@ -8,22 +8,23 @@
                              pkgname) {
             
             ## Set brigestan and cmdstanr environment variables / directories 
-            bs_dir <- bridgestan_path()
-            cmdstan_dir <- cmdstanr_path()
+            ##  bs_dir <- bridgestan_path()
+            cmdstan_dir <- BayesMVP:::cmdstanr_path()
   
             if (.Platform$OS.type == "windows") {
               
               TBB_STAN_1 <- TBB_STAN_2 <- TBB_CMDSTAN_DLL <- DUMMY_MODEL_SO_1 <- DUMMY_MODEL_SO_2 <- DUMMY_MODEL_DLL_1 <- DUMMY_MODEL_DLL_2 <- NULL
               
-                          ### TBB_BS <- file.path(system.file(package = "BayesMVP"), "tbb_bs", "tbb.dll") # not needed?
+              ### TBB_BS <- file.path(system.file(package = "BayesMVP"), "tbb_bs", "tbb.dll") 
               try({   TBB_STAN_1 <- file.path(libname, pkgname, "inst", "BayesMVP", "inst", "tbb_stan", "tbb.dll")   })
               try({   TBB_CMDSTAN_DLL <- file.path(cmdstan_dir, "stan", "lib", "stan_math", "lib", "tbb", "tbb.dll")    }) # prioritise user's installed tbb dll/so
-              try({   DUMMY_MODEL_SO_1 <- file.path(libname, pkgname, "inst", "BayesMVP", "inst", "libs", "x64", "dummy_stan_model_win_model.so")   })
+              try({   DUMMY_MODEL_SO_1 <- file.path(libname, pkgname, "inst", "BayesMVP", "inst",  "dummy_stan_model_win_model.so")   })
               try({   DUMMY_MODEL_DLL_1 <- file.path(libname, pkgname, "inst", "BayesMVP","inst",  "dummy_stan_model_win_model.dll")   })
                           
-                          dll_paths <- c(TBB_STAN_1, TBB_STAN_2, TBB_CMDSTAN_DLL,
-                                         DUMMY_MODEL_SO_1, DUMMY_MODEL_SO_2,
-                                         DUMMY_MODEL_DLL_1, DUMMY_MODEL_DLL_2)
+                          dll_paths <- c(TBB_STAN_1,  
+                                         TBB_CMDSTAN_DLL,
+                                         DUMMY_MODEL_SO_1, 
+                                         DUMMY_MODEL_DLL_1)
                     
               
             } else {  ### if Linux or Mac
@@ -34,9 +35,9 @@
                 try({   TBB_CMDSTAN_SO <- file.path(cmdstan_dir, "stan", "lib", "stan_math", "lib", "tbb", "libtbb.so.2")    }) # prioritise user's installed tbb dll/so
                 try({   DUMMY_MODEL_SO_1 <- file.path(libname, pkgname, "inst", "BayesMVP", "inst", "dummy_stan_model_model.so")   })
                           
-                          dll_paths <- c(TBB_STAN_1, TBB_STAN_2, TBB_CMDSTAN_SO,
-                                         DUMMY_MODEL_SO_1, DUMMY_MODEL_SO_2)
-                        
+                          dll_paths <- c(TBB_STAN_1, 
+                                         TBB_CMDSTAN_SO,
+                                         DUMMY_MODEL_SO_1)
               
             }
                       # Attempt to load each DLL / SO
