f2e163734ac32a11aace05d58d37403ac6017432
CerulloE1996 <80172000+CerulloE1996@users.noreply.github.com>
2025-01-24 13:18:26 +0000

    Delete inst/BayesMVP/src/Stan_functions directory

--- inst/BayesMVP/src/Stan_functions/approx_cpp_fns_for_Stan.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MVP_EXP_APPROX_HPP
-#define MVP_EXP_APPROX_HPP
-
-#include "approx_cpp_fns_for_Stan_prim.hpp"
-#include "approx_cpp_fns_for_Stan_rev.hpp"
-//// #include "mvp_exp_approx_fwd.hpp" /// this one is optional, I think
-
-#endif
