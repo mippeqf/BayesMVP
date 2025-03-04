a80d40fc436992bf63d837138db5f428d6bec03c
CerulloE1996 <80172000+CerulloE1996@users.noreply.github.com>
2025-03-04 18:19:54 +0000

    Add files via upload

--- inst/BayesMVP/src/eigen_config.hpp
+++ inst/BayesMVP/src/eigen_config.hpp
@@ -1,52 +1,48 @@
 #pragma once 
 
-
 #ifndef EIGEN_CONFIG_H
 #define EIGEN_CONFIG_H
 
-
+// Basic configuration
 #define EIGEN_NO_DEBUG
 #define EIGEN_DONT_PARALLELIZE
- 
 
-#ifdef EIGEN_MAX_ALIGN_BYTES
-    #define EIGEN_MAX_ALIGN_BYTES_OLD EIGEN_MAX_ALIGN_BYTES
-    #undef EIGEN_MAX_ALIGN_BYTES
+// Define EIGEN_MAX_ALIGN_BYTES if it's not already defined
+#ifndef EIGEN_MAX_ALIGN_BYTES
+#define EIGEN_MAX_ALIGN_BYTES 16  // Default value
 #endif
- 
 
+// Modify alignment based on architecture
 #ifdef _WIN32
-        //  #define EIGEN_DONT_ALIGN_STATICALLY
-      #if defined(USE_AVX_512) //// use AVX-512 if available
+        #if defined(USE_AVX_512)
+            #undef EIGEN_MAX_ALIGN_BYTES
             #define EIGEN_VECTORIZE_AVX512
             #define EIGEN_MAX_ALIGN_BYTES 64
-      #elif defined(USE_AVX2)  //// use AVX2 (if AVX-512 NOT available)
+        #elif defined(USE_AVX2)
+            #undef EIGEN_MAX_ALIGN_BYTES
             #define EIGEN_VECTORIZE_AVX2
             #define EIGEN_MAX_ALIGN_BYTES 32
-      #elif defined(USE_AVX)   //// use AVX (if both AVX-512 and AVX2 NOT available)
-            #define EIGEN_VECTORIZE_AVX
-            #define EIGEN_MAX_ALIGN_BYTES 16 
-      #else 
-            #define EIGEN_MAX_ALIGN_BYTES EIGEN_MAX_ALIGN_BYTES_OLD
-      #endif
-#else
-      #if defined(USE_AVX_512) //// use AVX-512 if available
-            #define EIGEN_VECTORIZE_AVX512
-            #define EIGEN_MAX_ALIGN_BYTES 64
-      #elif defined(USE_AVX2)  //// use AVX2 (if AVX-512 NOT available)
-            #define EIGEN_VECTORIZE_AVX2
-            #define EIGEN_MAX_ALIGN_BYTES 32
-      #elif defined(USE_AVX)   //// use AVX (if both AVX-512 and AVX2 NOT available)
+        #elif defined(USE_AVX)
+            #undef EIGEN_MAX_ALIGN_BYTES
             #define EIGEN_VECTORIZE_AVX
             #define EIGEN_MAX_ALIGN_BYTES 16
-      #else 
-            #define EIGEN_MAX_ALIGN_BYTES EIGEN_MAX_ALIGN_BYTES_OLD
-      #endif
+        #endif
+#else  // Non-Windows (Linux, MacOS, etc.)
+        #if defined(USE_AVX_512)
+            #undef EIGEN_MAX_ALIGN_BYTES
+            #define EIGEN_VECTORIZE_AVX512
+            #define EIGEN_MAX_ALIGN_BYTES 64
+        #elif defined(USE_AVX2)
+            #undef EIGEN_MAX_ALIGN_BYTES
+            #define EIGEN_VECTORIZE_AVX2
+            #define EIGEN_MAX_ALIGN_BYTES 32
+        #elif defined(USE_AVX)
+            #undef EIGEN_MAX_ALIGN_BYTES
+            #define EIGEN_VECTORIZE_AVX
+            #define EIGEN_MAX_ALIGN_BYTES 16
+        #endif
 #endif
 
 
 
-
-
-#endif
-     
+#endif // end of EIGEN_CONFIG_H
